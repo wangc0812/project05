b2470548702d6ab3a4eb37a15f244393a7c3c7a2
Leon Wang <1091803778@qq.com>
2022-12-05 16:34:36 +0800

    1205

--- inc/matrix.hpp
+++ inc/matrix.hpp
@@ -21,17 +21,21 @@ public:
     // Constructor
     Matrix()
     {
-        release();
+        _row = 0;
+        _column = 0;
+        _size = 0;
+        _elem = NULL;
         std::cout << "default constructor used" << std::endl; 
     }
 
+    // this constructor is to construct a full zero matrix with given size
     Matrix(size_t r, size_t c) : _row(r), _column(c)
     {
         if (r == 0 || c == 0 )
         {
             ERROR_INPUT_INPUTPARA;
-            std::cout << "This error happened in constructor" << std::endl;
-            return NULL;
+            std::cerr << "This error happened in constructor" << std::endl;
+            return;
         }
         else
         {
@@ -40,9 +44,10 @@ public:
 
             if (_elem == NULL)
             {
-                std::cout << "failed to allocate memory for matrix data" << std::endl;
-                free(_elem);
-                return NULL;
+                std::cerr << "Matrix(size_t r, size_t c) : failed to allocate memory for matrix data" << std::endl;
+                delete[] _elem;
+                _elem = NULL;
+                return;
             }
             
             memset(_elem, 0, _size * sizeof(T));
@@ -52,9 +57,50 @@ public:
 
     }
 
-    Matrix(size_t r, size_t c, * data)
+    Matrix(size_t r, size_t c, T * data) : _row(r), _column(c)
+    {
+        if (r == 0 || c == 0 )
+        {
+            ERROR_INPUT_INPUTPARA;
+            std::cerr << "This error happened in constructor" << std::endl;
+            return;
+        }
 
-    Matrix(const Matrix &rhs);
+        if (data == NULL)
+        {
+            std::cerr << "input data invalid" << std::endl;
+            return;
+        }
+        
+        this -> _size = 0;
+        this -> _elem = NULL;
+
+        size_t size = this -> _column * this -> _row;
+        create(size, data);
+
+        std::cout << "constructor with data used" << std::endl;
+
+    }
+
+    Matrix(const Matrix<T> & rhs)
+    {
+    
+        if(rhs._elem == NULL)
+        {
+         std::cerr << "Matrix(const Matrix<T> & rhs) : rhs data invalid" << std::endl; 
+         return;  
+        }
+        
+        this -> _size = 0;
+        this -> _elem = NULL;
+
+        create(rhs._size, rhs._elem);
+        this -> _column = rhs._column;
+        this -> _row = rhs._row;
+
+        std::cout << "copy constructor used" << std::endl; 
+
+    }
     
     bool create(size_t size,  const T * data)
     {
@@ -62,13 +108,13 @@ public:
 
         if (size == 0)
         {
-            std::cout << "ERROR: matrix size is zero" << std::endl;
+            std::cerr << "ERROR: matrix size is zero" << std::endl;
             return false;
         }
 
         if(data == NULL)
         {
-            std::cout << "input data invalid" << std::endl;
+            std::cerr << "input data invalid" << std::endl;
             return false;
         }
         
@@ -81,8 +127,9 @@ public:
 
         if (_elem == NULL)
         {
-            std::cout << "failed to allocate memory for matrix data" << std::endl;
-            free( _elem);
+            std::cerr << "failed to allocate memory for matrix data" << std::endl;
+            delete[] _elem;
+            _elem = NULL;
             return false;
         }
 
@@ -109,12 +156,7 @@ public:
     // Deconstructor
     ~Matrix()
     {
-        if (_elem != NULL)
-        {
-            delete[] _elem;
-            _elem = NULL;
-        }
-
+        release();
         std::cout << "deconstructor used" << std::endl;
             
     }
@@ -122,7 +164,7 @@ public:
     // Print Matrix or Elems
     void printMatrix() const
     {
-        if(this != NULL)
+        if(this->_elem != NULL)
         {
             std::cout<< "---------------Matrix Print Start----------------" <<std::endl;
             
@@ -142,18 +184,24 @@ public:
         else
         {
             ERROR_INPUT_POINTER;
-            std::cout << "This error happened in printMatrix()" << std::endl;
+            std::cerr << "This error happened in printMatrix()" << std::endl;
         }
 
     }
 
-     T getElement(size_t r, size_t c) const
+    T getElement(size_t r, size_t c) const
     {
         if ( r >= this->_row || c >= this->_column )
         {
             std::cerr << "getElement(): Indices are out of range" << std::endl;
-            return 0;
+            return 1;
         }
+        if (this -> _elem == NULL)
+        {
+            std::cerr << "getElement(): input matrix data invalid" << std::endl;
+            return 1;
+        }
+
         return _elem[ this->_column * r + c];
     }
 
@@ -165,67 +213,81 @@ public:
             return false;
         }
 
+        if (this -> _elem == NULL)
+        {
+            std::cerr << "setElement(): input matrix data invalid" << std::endl;
+            return false;
+        }
+        
         _elem[ this->_column * r + c] = value;
         return true;
     }
 
     // Operator overloading
-    Matrix operator+(const Matrix<T> & B) const
+    Matrix<T> operator+(const Matrix<T> & B) const
     {
-        Matrix<T> MAT_SUM;
+        if (B._elem == NULL || this -> _elem == NULL)
+        {   
+            std::cerr << "matrix data invalid" << std::endl;
+            std::cerr << "This error happened operator+" << std::endl;
+            return *this;
+        }
+        
         if (this ->_column != B._column || this ->_row != B._row )
         {
             ERROR_INPUT_INPUTPARA;
-            std::cout << "This error happened operator+" << std::endl;
-            return MAT_SUM;
+            std::cerr << "This error happened operator+" << std::endl;
+            return *this;
         }
-        else
-        {
-            MAT_SUM._column = this -> _column;
-            MAT_SUM._row = this -> _row;
-            MAT_SUM._size = this -> _size;
-            MAT_SUM._elem = new T[_size];
-            memset(MAT_SUM._elem, 0, MAT_SUM._size * sizeof(T));
+        
+        Matrix<T> MAT_SUM(this -> _column, this -> _row);
             
-
-            for(size_t i = 0; i < this ->_size; i++)
-            {
-                MAT_SUM._elem[i] = this -> _elem[i] + B._elem[i];
-            }
-            return MAT_SUM;
+        for(size_t i = 0; i < this ->_size; i++)
+        {
+            MAT_SUM._elem[i] = this -> _elem[i] + B._elem[i];
         }
+
+        return MAT_SUM;
+  
 
     }
 
-    Matrix operator-(const Matrix<T> & B) const
+    Matrix<T> operator-(const Matrix<T> & B) const
     {
-        Matrix<T> MAT_SUB;
+        if (B._elem == NULL || this -> _elem == NULL)
+        {   
+            std::cerr << "matrix data invalid" << std::endl;
+            std::cerr << "This error happened operator-" << std::endl;
+            return *this;
+        }
+
         if (this ->_column != B._column || this ->_row != B._row )
         {
             ERROR_INPUT_INPUTPARA;
-            std::cout << "This error happened operator-" << std::endl;
-            return MAT_SUB;
+            std::cerr << "This error happened operator-" << std::endl;
+            return *this;
         }
-        else
-        {
-            MAT_SUB._column = this -> _column;
-            MAT_SUB._row = this -> _row;
-            MAT_SUB._size = this -> _size;
-            MAT_SUB._elem = new T[_size];
-            memset(MAT_SUB._elem, 0, MAT_SUB._size * sizeof(T));
-            
 
-            for(size_t i = 0; i < this ->_size; i++)
-            {
-                MAT_SUB._elem[i] = this -> _elem[i] - B._elem[i];
-            }
-            return MAT_SUB;
+        Matrix<T> MAT_SUB(this -> _column, this -> _row);
+
+        for(size_t i = 0; i < this ->_size; i++)
+        {
+            MAT_SUB._elem[i] = this -> _elem[i] - B._elem[i];
         }
+        return MAT_SUB;
+        
 
     }
 
     Matrix operator*(const T scalar) const
     {
+        if (this -> _elem == NULL)
+        {   
+            std::cerr << "matrix data invalid" << std::endl;
+            std::cerr << "This error happened in Matrix operator*(const T scalar)" << std::endl;
+            return *this;
+        }
+        
         Matrix<T> DOT_PRODUCT(this -> _column, this -> _row);
 
         for(size_t i = 0; i < this ->_size; i++)
